71a815d5d5659a99f219639deffe520cf79fd0f8
anthonykizer <anthony.kizer88@gmail.com>
2022-01-23 22:32:16 -0800

    added fundamentals to combo selection

--- src/combinations/fundamentals.js
+++ src/combinations/fundamentals.js
@@ -1,64 +1,42 @@
 export const combos = [
     {
-        name: "1-2 Magic",
+        name: "Fundamentals",
         combos: [
             {
                 sequence: ["1", "2"],
             },
             {
-                sequence: ["1", "1", "2", "SideStep", "2", "3"],
+                sequence: ["1", "1", "2"],
             },
             {
-                sequence: ["1", "1", "2", "SideStep", "3", "2"]
+                sequence: ["1", "2b"]
             },
             {
-                sequence: ["1", "1", "2b"],
-            },
-
-            {
-                sequence: ["1", "2", "Slip"],
+                sequence: ["1", "2", "3", "2"],
             },
             {
-                sequence: ["1", "1", "Slip", "1", "2"],
+                sequence: ["5", "2", "3", "2"]
             },
             {
-                sequence: ["Slip", "Slip", "1", "1", "2"]
+                sequence: ["1b", "6"]
             },
             {
-                sequence: ["1", "2", "Duck", "2", "Roll"]
-            }
-        ]
-    },
-    {
-        name: "Groundwork Offense",
-        combos: [
-            {
-                sequence: ["1", "2", "3", "4", "3", "4"]
+                sequence: ["1", "2", "3b", "4b", "3", "4"]
             },
             {
-                sequence: ["1", "2", "3", "3", "3"]
+                sequence: ["1", "2", "Pull", "2", "3"]
             },
             {
-                sequence: ["1", "2", "1", "1", "2"]
+                sequence: ["1", "2", "3", "Roll"]
             },
             {
-                sequence: ["1", "2", "Pivot", "1", "1", "2"]
+                sequence: ["Slip", "1", "2"]
             },
             {
-                sequence: ["1", "2", "Roll", "6", "3", "2"]
-            },
-            {
-                sequence: ["1", "2", "5", "4"]
-            },
-            {
-                sequence: ["1", "2", "Slip", "Roll", "3b", "5"]
-            },
-            {
-                sequence: ["1", "5", "2", "Roll", "2", "3"]
+                sequence: ["Slip", "2", "3"]
             }
         ]
     }
-
 
 ]
 
